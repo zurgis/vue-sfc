fe45f0cc19cb5f8d7f775a14498b7a230948ad62
Anatoly Dormidontov <anatolydormidontov@Anatolys-MacBook-Air.local>
2022-07-18 11:43:35 +0300

    pagination component

--- src/components/VPagination/VPagination.vue
+++ src/components/VPagination/VPagination.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import arrowLeftImg from './icons/arrow_left.svg'
 import arrowRightImg from './icons/arrow_right.svg'
 import VButton from '../VButton.vue'
 
+interface IPages {
+  [index: number]: {
+    value: number
+    selected: boolean
+  } | {
+    ellipsis: boolean
+  }
+}
+
 const props = defineProps<{
-  count: number 
+  count: number
   perPage: number
+  totalVisible?: number
+}>()
+
+const emit = defineEmits<{
+  (e: 'click', value: number): void
 }>()
 
 const icons = {
@@ -14,31 +28,91 @@ const icons = {
   right: { size: "xs" as const, src: arrowRightImg }
 }
 
-const ellipsis = '...'
+const length = Math.floor(props.count / props.perPage)
+const totalVisible = props.totalVisible && props.totalVisible > 2 ? props.totalVisible : 3
 
-const page = 9
+const selected = ref(1)
 
-const range = computed(() => {
-  if (props.count <= 0) return []
+const items = computed(() => {
+  let pages: IPages = {}
 
-  const even = props.count % 2 === 0 // false
-  const middle = even ? props.count / 2 : Math.floor(props.count / 2) // 7
-  const left = even ? middle : middle + 1 // 8
-  const right = props.count - middle // 8
+  function addPage(index: number) {
+    const page = {
+      value: index + 1, 
+      selected: index === (selected.value - 1)
+    }
 
-  if (left - page >= 0) {
-    return [Array.from({ length: 3 }, (v: number = 1, k) => v + k), ellipsis, props.count]
-  } else if (page - right >= 0) {
-    const rangeStart = props.count - 2
-    return [1, ellipsis, Array.from({ length: 3 }, (v: number = rangeStart, k) => v + k)]
-  } else {
-    const rangeLength = Math.max(1, 3 - 3)
-    console.info(rangeLength)
-    const rangeStart = rangeLength === 1 ? props.count : props.count - Math.ceil(rangeLength / 2) + 1
-
-    return [1, ellipsis, Array.from({ length: rangeLength }, (v: number = rangeStart, k) => v + k), ellipsis, props.count]
+    pages[index] = page 
   }
+
+  if (length <= totalVisible) {
+    for (let index = 0; index < length; index++) {
+      addPage(index)
+    }
+  } 
+  else {
+    const middle = Math.floor(totalVisible / 2)
+
+    function setEllipsis(index: number) {
+      const ellipsis = {
+        ellipsis: true
+      }
+
+      pages[index] = ellipsis
+    }
+
+    // Add first page
+    addPage(0)
+
+    let left = 0
+    if (selected.value - middle > 0) {
+      left = selected.value - 1 - middle
+    }
+
+    let right = left + totalVisible - 1
+    if (right >= length) {
+      right = length - 1
+      left = right - totalVisible + 1
+    }
+
+    if (left > 1) {
+      setEllipsis(left - 1)
+    }
+
+    for (let index = left; index <= right && index < length - 1; index++) {
+      addPage(index)
+    }
+
+    if (right + 1 < length - 1) {
+      setEllipsis(right + 1)
+    }
+
+    // Add last page
+    addPage(length - 1)
+  }
+
+  return pages
 })
+
+function handlePageSelected(index: number) {
+  if (selected.value === index) return
+
+  selected.value = index
+  
+  emit('click', selected.value)
+}
+
+function prevPage() {
+  if (selected.value <= 1) return
+
+  handlePageSelected(selected.value - 1)
+}
+
+function nextPage() {
+  if (selected.value >= props.count) return
+
+  handlePageSelected(selected.value + 1)
+}
 </script>
 
 <template>
@@ -46,17 +120,30 @@ const range = computed(() => {
     <VButton 
       :appearance="'flat'"
       :icon="icons.left"
+      @click="prevPage"
     />
 
-    <VButton 
-      v-for="item in range"
-      :appearance="'flat'"
-      :value="String(item)"
-    />
+    <template v-for="item in items">
+      <VButton 
+        v-if="'ellipsis' in item"
+        :appearance="'flat'"
+        :value="'...'"
+        :disabled="true"
+      />
+
+      <VButton 
+        v-else
+        :appearance="'flat'"
+        :value="String(item.value)"
+        :class="item.selected ? 'selected': null"
+        @click="handlePageSelected(item.value)"
+      />
+    </template>
 
     <VButton 
       :appearance="'flat'"
       :icon="icons.right"
+      @click="nextPage"
     />
   </div>
 </template>
@@ -65,5 +152,9 @@ const range = computed(() => {
 .pagination {
   display: flex;
   flex-direction: row;
+
+  >.selected {
+    background-color: #0000000a;
+  }
 }
 </style>
--- src/views/Pagination.vue
+++ src/views/Pagination.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 import VPagination from '../components/VPagination/VPagination.vue'
-import VPagination1 from '../components/VPagination/VPagination1.vue'
 </script>
 
 <template>
   <div>
-    <VPagination :count="15" :per-page="10" />
-
-    <VPagination1 :count="15"/>
+    <VPagination :count="100" :per-page="10" />
   </div>
 </template>
 
